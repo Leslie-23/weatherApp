7d787517db2161db429d3e19ac655e3687b6c743
leslie-23 <leslieajayi27@gmail.com>
2024-05-10 10:41:01 +0000

    new adds

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Header />
-    <DisplayCountriesWeather />
     <App />
+    <DisplayCountriesWeather />
     {/* <Footer /> */}
   </React.StrictMode>
 );
